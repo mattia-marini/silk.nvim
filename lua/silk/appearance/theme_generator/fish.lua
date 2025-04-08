aac5572c0da2250e6535a870b298c6ad273c4669
Mattia Marini <marmattia26@gmail.com>
2025-04-08 16:30:18 +0200

    Major improvements in fish theme generation

--- lua/silk/appearance/theme_generator/fish.lua
+++ lua/silk/appearance/theme_generator/fish.lua
@@ -1,87 +1,115 @@
 -- Define mapping of Fish variables to Neovim highlight groups
 local fish_to_nvim = {
-  fish_color_autosuggestion = "Comment",
-  fish_color_cancel = "Error",
-  fish_color_command = "Statement",
-  fish_color_comment = "Comment",
-  fish_color_cwd = "Directory",
-  fish_color_cwd_root = "Directory",
-  fish_color_end = "Delimiter",
-  fish_color_error = "Error",
-  fish_color_escape = "Special",
-  fish_color_history_current = "Search",
-  fish_color_host = "Identifier",
-  fish_color_host_remote = "Identifier",
-  fish_color_keyword = "Keyword",
-  fish_color_match = "IncSearch",
-  fish_color_normal = "Normal",
-  fish_color_operator = "Operator",
-  fish_color_option = "Type",
-  fish_color_param = "Identifier",
-  fish_color_quote = "String",
-  fish_color_redirection = "Special",
-  fish_color_search_match = "Search",
-  fish_color_selection = "Visual",
-  fish_color_status = "ErrorMsg",
-  fish_color_user = "Constant",
-  fish_color_valid_path = "Underlined",
-  fish_pager_color_background = "Pmenu",
-  fish_pager_color_completion = "PmenuSel",
-  fish_pager_color_description = "Pmenu",
-  fish_pager_color_prefix = "Search",
-  fish_pager_color_progress = "StatusLine",
-  fish_pager_color_secondary_background = "PmenuSbar",
-  fish_pager_color_secondary_completion = "NonText",
-  fish_pager_color_secondary_description = "Pmenu",
-  fish_pager_color_secondary_prefix = "Search",
-  fish_pager_color_selected_background = "PmenuSel",
-  fish_pager_color_selected_completion = "PmenuSel",
-  fish_pager_color_selected_description = "PmenuSel",
-  fish_pager_color_selected_prefix = "PmenuSel"
+  fish_color_autosuggestion = { hi_group = "Comment" },
+  fish_color_cancel = { hi_group = "Error" },
+  fish_color_command = { hi_group = "Statement" },
+  fish_color_comment = { hi_group = "Comment" },
+  fish_color_cwd = { hi_group = "Directory" },
+  fish_color_cwd_root = { hi_group = "Directory" },
+  fish_color_end = { hi_group = "Delimiter" },
+  fish_color_error = { hi_group = "Error" },
+  fish_color_escape = { hi_group = "Special" },
+  fish_color_history_current = { hi_group = "Search" },
+  fish_color_host = { hi_group = "Identifier" },
+  fish_color_host_remote = { hi_group = "Identifier" },
+  fish_color_keyword = { hi_group = "Keyword" },
+  fish_color_match = { hi_group = "IncSearch" },
+  fish_color_normal = { hi_group = "Normal", attr = { "fg" } },
+  fish_color_operator = { hi_group = "Operator" },
+  fish_color_option = { hi_group = "Type" },
+  fish_color_param = { hi_group = "Identifier" },
+  fish_color_quote = { hi_group = "String" },
+  fish_color_redirection = { hi_group = "Special" },
+  fish_color_search_match = { hi_group = "Search" },
+  fish_color_selection = { hi_group = "Visual" },
+  fish_color_status = { hi_group = "ErrorMsg" },
+  fish_color_user = { hi_group = "Constant" },
+  fish_color_valid_path = { hi_group = "Underlined" },
+  --
+  fish_pager_color_completion = { hi_group = "Pmenu", attr = { fg = true } },
+  fish_pager_color_selected_completion = { hi_group = "PmenuSel", attr = { fg = true } },
+  fish_pager_color_secondary_completion = {
+    hi_group = "Pmenu",
+    attr = { fg = true },
+    -- extra = { "--dim" }
+  },
+  --
+  fish_pager_color_background = { hi_group = "Pmenu", attr = {} },
+  fish_pager_color_selected_background = { hi_group = "PmenuSel", attr = { "bg" } },
+  fish_pager_color_secondary_background = {
+    hi_group = "Pmenu",
+    attr = {},
+  },
+  --
+  fish_pager_color_description = { hi_group = "Pmenu", attr = { fg = true } },
+  fish_pager_color_selected_description = { hi_group = "PmenuSel", attr = { fg = true } },
+  fish_pager_color_secondary_description = {
+    hi_group = "Pmenu",
+    attr = { fg = true },
+    -- extra = { "--dim" }
+  },
+  --
+  fish_pager_color_prefix = { hi_group = "Pmenu", attr = { fg = true }, extra = { "--underline", "--bold" } },
+  fish_pager_color_selected_prefix = { hi_group = "PmenuSel", attr = { fg = true }, extra = { "--underline", "--bold" } },
+  fish_pager_color_secondary_prefix = {
+    hi_group = "Pmenu",
+    attr = { fg = true },
+    extra = { "--underline", "--bold" }
+  },
+  --
+  fish_pager_color_progress = { hi_group = "StatusLine", attr = { fg = true } },
 }
 
+local function format_color(color)
+  return string.format("%06x", color)
+end
+
+local function trim(s)
+  return s:match("^%s*(.-)%s*$")
+end
+
 -- Function to convert Neovim highlight attributes to Fish-style options
-local function format_attrs(attrs)
-  local parts = {}
+local function format_attrs(hi_group, attr)
+  local style = {}
 
-
-  if attrs.bold then table.insert(parts, "--bold") end
-  if attrs.italic then table.insert(parts, "--italics") end
-  if attrs.reverse then table.insert(parts, "--reverse") end
-  if attrs.underline then table.insert(parts, "--underline") end
+  if hi_group.bg and (not attr or attr.bg) then table.insert(style, "--background=" .. format_color(hi_group.bg)) end
+  if hi_group.bold and (not attr or attr.bold) then table.insert(style, "--bold") end
+  if hi_group.italic and (not attr or attr.italic) then table.insert(style, "--italics") end
+  if hi_group.reverse and (not attr or attr.reverse) then table.insert(style, "--reverse") end
+  if hi_group.underline and (not attr or attr.underline) then table.insert(style, "--underline") end
   -- if attrs.standout then table.insert(parts, "--background") end -- rough match
   -- if attrs.strikethrough then table.insert(parts, "--dim") end   -- not exact
 
-  return table.concat(parts, " ")
+  return table.concat(style, " ")
 end
 
 function generate_theme()
   local rv = {}
-  for fish_var, nvim_group in pairs(fish_to_nvim) do
-    local attrs = vim.api.nvim_get_hl(0, { name = nvim_group, create = false })
-    if attrs then
-      local hex = ""
-      if attrs.fg then
-        hex = string.format("%06x", attrs.fg)
+  for fish_var, format in pairs(fish_to_nvim) do
+    local hi_group = vim.api.nvim_get_hl(0, { name = format.hi_group, create = false })
+    if hi_group then
+      -- Fetching optional fg
+      local fg = ""
+      if not format.attr or format.attr.fg then
+        if hi_group.fg then
+          fg = format_color(hi_group.fg)
+        end
       end
 
-      local flags = format_attrs(attrs)
+      -- Parseing additional formatting flags
+      local flags = format_attrs(hi_group, format.attr)
 
-      local line = fish_var .. " " .. hex
-      if #flags > 0 then
-        line = line .. " " .. flags
-      end
+      -- Parsing extra flags
+      local extras = table.concat(format.extra or {}, " ")
 
-      -- if file then
-      --   file:write(line .. "\n")
-      -- end
+      -- Constructing the Fish variable line
+      local line = fish_var .. " " .. fg .. " " .. flags .. " " .. extras
+      line = trim(line)
+
       table.insert(rv, line)
     else
-      -- if file then
-      --   file:write(fish_var .. " error\n")
-      -- end
+      -- Inserting error line to indicate missing highlight group. This should happen
       table.insert(rv, fish_var .. " error\n")
-      -- print(fish_var .. " error\n")
     end
   end
   return rv
@@ -89,6 +117,10 @@ end
 
 function generate_and_write_theme()
   local rv = generate_theme()
+  table.insert(rv, 1, "# name: '" .. vim.g.colors_name .. "'")
+  table.insert(rv, 2,
+    "# preferred_background: " .. string.format("%06X", vim.api.nvim_get_hl(0, { name = "Normal" }).bg))
+  table.insert(rv, 3, "")
 
   local job = vim.system({ 'fish', '-c', 'echo $__fish_config_dir' }, { text = true }):wait()
   if not (job.code == 0 and #job.stdout > 0) then
